8a01bee9192fecdc33d20642a6e129459411932c
hop- <hovhannespalyan@openmailbox.org>
2015-05-03 17:17:23 +0400

    now engine uses pointers of created objects

--- src/world.cpp
+++ src/world.cpp
@@ -258,42 +258,42 @@ inline void MovableObject::setV(Vector v)
 
 ////////////////////////////////////////////////////////////////
 
-inline bool areIntersected(const ObjectInWorld& o1,
-                           const ObjectInWorld& o2)
+inline bool areIntersected(const ObjectInWorld* o1,
+                           const ObjectInWorld* o2)
 {
-    return (o1.left() < o2.right() &&
-        o1.right() > o2.left() &&
-        o1.bottom() < o2.top() &&
-        o1.top() > o2.bottom());
+    return (o1->left() < o2->right() &&
+        o1->right() > o2->left() &&
+        o1->bottom() < o2->top() &&
+        o1->top() > o2->bottom());
 }
 
-inline PositionUnit getIntersectedX(const ObjectInWorld& o1, const ObjectInWorld& o2)
+inline PositionUnit getIntersectedX(const ObjectInWorld* o1, const ObjectInWorld* o2)
 {
     PositionUnit d1x, d2x;
-    d1x = o1.right() - o2.right() < 0 ? 0 : o1.right() - o2.right();
-    d2x = o1.left() - o2.left() < 0 ? 0 : o1.left() - o2.left();
-    return o1.right() - o2.left() - d1x - d2x;
+    d1x = o1->right() - o2->right() < 0 ? 0 : o1->right() - o2->right();
+    d2x = o1->left() - o2->left() < 0 ? 0 : o1->left() - o2->left();
+    return o1->right() - o2->left() - d1x - d2x;
 }
 
-inline PositionUnit getIntersectedY(const ObjectInWorld& o1, const ObjectInWorld& o2)
+inline PositionUnit getIntersectedY(const ObjectInWorld* o1, const ObjectInWorld* o2)
 {
     PositionUnit d1y, d2y;
-    d1y = o1.top() - o2.top() < 0 ? 0 : o1.top() - o2.top();
-    d2y = o1.bottom() - o2.bottom() < 0 ? 0 : o1.bottom() - o2.bottom();
-    return o1.top() - o2.bottom() - d1y - d2y;
+    d1y = o1->top() - o2->top() < 0 ? 0 : o1->top() - o2->top();
+    d2y = o1->bottom() - o2->bottom() < 0 ? 0 : o1->bottom() - o2->bottom();
+    return o1->top() - o2->bottom() - d1y - d2y;
 }
 
-inline bool keepObjectInSurface(MovableObject& o1, const ObjectInWorld& o2)
+inline bool keepObjectInSurface(MovableObject* o1, const ObjectInWorld* o2)
 {
     PositionUnit stepsBackForX = std::abs(getIntersectedX(o1, o2) /
-                std::cos(o1.getV().getAngleInRadians()));
+                std::cos(o1->getV().getAngleInRadians()));
     PositionUnit stepsBackForY = std::abs(getIntersectedY(o1, o2) /
-                std::sin(o1.getV().getAngleInRadians()));
+                std::sin(o1->getV().getAngleInRadians()));
     if (stepsBackForX < stepsBackForY) {
-        o1.backPosition(-stepsBackForX - 1);
+        o1->backPosition(-stepsBackForX - 1);
         return true;
     } else {
-        o1.backPosition(-stepsBackForY - 1);
+        o1->backPosition(-stepsBackForY - 1);
         return false;
     }
 }
@@ -331,51 +331,51 @@ World::World(float cOfEnvResistance,
     m_hasBeenChanged(true)
 {}
 
-void World::setWalls(std::vector<Wall> walls)
+void World::setWalls(std::vector<Wall*> walls)
 {
     m_walls = walls;
 }
 
-void World::setMovables(std::vector<MovableObject> movables)
+void World::setMovables(std::vector<MovableObject*> movables)
 {
     m_movables = movables;
 }
 
-int World::add(Wall wall)
+int World::add(Wall* wall)
 {
     m_walls.push_back(wall);
     return m_walls.size() - 1 ;
 }
 
-int World::add(MovableObject movable)
+int World::add(MovableObject* movable)
 {
     m_movables.push_back(movable);
     return m_movables.size() - 1;
 }
 
-const std::vector<Wall>& World::getWalls() const
+const std::vector<Wall*>& World::getWalls() const
 {
     return m_walls;
 }
 
-const std::vector<MovableObject>& World::getMovables() const
+const std::vector<MovableObject*>& World::getMovables() const
 {
     return m_movables;
 }
 
-Wall World::getWall(int index) const
+Wall* World::getWall(int index) const
 {
     return m_walls[index];
 }
 
-MovableObject World::getMovable(int index) const
+MovableObject* World::getMovable(int index) const
 {
     return m_movables[index];
 }
 
 void World::accelerate(int index, Vector a)
 {
-    m_movables[index].accelerate(a, m_coefficientOfSlowdown);
+    m_movables[index]->accelerate(a, m_coefficientOfSlowdown);
 }
 
 bool World::hasBeenChanged()
@@ -408,37 +408,37 @@ short World::getCoefficientOfSlowdown()
     return 1 / m_coefficientOfSlowdown;
 }
 
-void World::updatePosition(MovableObject& object)
+void World::updatePosition(MovableObject* object)
 {
-    object.updatePosition(m_coefficientOfSlowdown);
+    object->updatePosition(m_coefficientOfSlowdown);
     objectCollisionDetection(object);
     Vector acceleration = m_gravityAcceleration;
     acceleration += getEnvResistanceEffects(object);
-    object.accelerate(acceleration, m_coefficientOfSlowdown);
+    object->accelerate(acceleration, m_coefficientOfSlowdown);
 }
 
-Vector World::getEnvResistanceEffects(const MovableObject& object)
+Vector World::getEnvResistanceEffects(const MovableObject* object)
 {
-    VectorUnit f_resistance = std::pow(object.getV().getMagnitude(), 2) *
-            object.getCoefficientOfResistance() *
+    VectorUnit f_resistance = std::pow(object->getV().getMagnitude(), 2) *
+            object->getCoefficientOfResistance() *
             m_coefficientOfResistanceOfEnvironment / 820000;
-    return Vector(f_resistance / object.getMass(), object.getV().getAngle() - 180);
+    return Vector(f_resistance / object->getMass(), object->getV().getAngle() - 180);
 }
 
-std::vector<MovableObject> World::getNearbyObjects(const MovableObject& object)
+std::vector<MovableObject*> World::getNearbyObjects(const MovableObject* object)
 {
     return m_movables;
 }
 
-std::vector<Wall> World::getNearbyWalls(const MovableObject& object)
+std::vector<Wall*> World::getNearbyWalls(const MovableObject* object)
 {
     return m_walls;
 }
 
-void World::objectCollisionDetection(MovableObject& object)
+void World::objectCollisionDetection(MovableObject* object)
 {
     for (int i = 0; i < static_cast<int>(m_movables.size()); ++i) {
-        if (&object != &m_movables[i] &&
+        if (object != m_movables[i] &&
             areIntersected(object, m_movables[i])) {
                calculateCollisionEffects(object, m_movables[i]); 
         }
@@ -450,56 +450,57 @@ void World::objectCollisionDetection(MovableObject& object)
     }
 }
 
-void World::calculateCollisionEffects(MovableObject& o1, MovableObject& o2)
+void World::calculateCollisionEffects(MovableObject* o1, MovableObject* o2)
 {
     keepObjectInSurface(o1, o2);
-    float e = (o1.getCoefficientOfElasticity() + o2.getCoefficientOfElasticity()) / 2;
+    float e = (o1->getCoefficientOfElasticity() + o2->getCoefficientOfElasticity()) / 2;
     Vector v1, v2;
-    v1.setX((VectorUnit(o1.getMass() * o1.getV().getX()) +
-        VectorUnit(o2.getMass() * o2.getV().getX()) +
-        VectorUnit(e * o2.getMass() * (o2.getV().getX() - o1.getV().getX()))) /
-        VectorUnit(o1.getMass() + o2.getMass()));
-    v1.setY((VectorUnit(o1.getMass() * o1.getV().getY()) +
-        VectorUnit(o2.getMass() * o2.getV().getY()) +
-        VectorUnit(e * o2.getMass() * (o2.getV().getY() - o1.getV().getY()))) /
-        VectorUnit((o1.getMass() + o2.getMass())));
-    v2.setX((VectorUnit(o1.getMass() * o1.getV().getX()) +
-        VectorUnit(o2.getMass() * o2.getV().getX()) +
-        VectorUnit(e * o1.getMass() * (o1.getV().getX() - o2.getV().getX()))) /
-        VectorUnit((o1.getMass() + o2.getMass())));
-    v2.setY((VectorUnit(o1.getMass() * o1.getV().getY()) +
-        VectorUnit(o2.getMass() * o2.getV().getY()) +
-        VectorUnit(e * o1.getMass() * (o1.getV().getY() - o2.getV().getY()))) /
-        VectorUnit(o1.getMass() + o2.getMass()));
-    o1.setV(v1);
-    o2.setV(v2);
+    v1.setX((VectorUnit(o1->getMass() * o1->getV().getX()) +
+        VectorUnit(o2->getMass() * o2->getV().getX()) +
+        VectorUnit(e * o2->getMass() * (o2->getV().getX() - o1->getV().getX()))) /
+        VectorUnit(o1->getMass() + o2->getMass()));
+    v1.setY((VectorUnit(o1->getMass() * o1->getV().getY()) +
+        VectorUnit(o2->getMass() * o2->getV().getY()) +
+        VectorUnit(e * o2->getMass() * (o2->getV().getY() - o1->getV().getY()))) /
+        VectorUnit((o1->getMass() + o2->getMass())));
+    v2.setX((VectorUnit(o1->getMass() * o1->getV().getX()) +
+        VectorUnit(o2->getMass() * o2->getV().getX()) +
+        VectorUnit(e * o1->getMass() * (o1->getV().getX() - o2->getV().getX()))) /
+        VectorUnit((o1->getMass() + o2->getMass())));
+    v2.setY((VectorUnit(o1->getMass() * o1->getV().getY()) +
+        VectorUnit(o2->getMass() * o2->getV().getY()) +
+        VectorUnit(e * o1->getMass() * (o1->getV().getY() - o2->getV().getY()))) /
+        VectorUnit(o1->getMass() + o2->getMass()));
+    o1->setV(v1);
+    o2->setV(v2);
 } 
 
 
-void World::calculateCollisionEffects(MovableObject& o, Wall& w)
+void World::calculateCollisionEffects(MovableObject* o, Wall* w)
 {
     float newAngle;
     if (keepObjectInSurface(o, w)) {
-        newAngle = 180 - o.getV().getAngle();
+        newAngle = 180 - o->getV().getAngle();
     } else {
-        newAngle = -o.getV().getAngle();
+        newAngle = -o->getV().getAngle();
     }
-    float e = (o.getCoefficientOfElasticity() + w.getCoefficientOfElasticity()) / 2;
-    float newMagnitude = e * o.getV().getMagnitude();
-    o.setV(Vector(newMagnitude, newAngle));
+    float e = (o->getCoefficientOfElasticity() + w->getCoefficientOfElasticity()) / 2;
+    float newMagnitude = e * o->getV().getMagnitude();
+    o->setV(Vector(newMagnitude, newAngle));
 }
 
 ////////////////////////////////////////////////////////////////
 
-ObjectDriver::ObjectDriver(MovableObject movable, World& world) :
+ObjectDriver::ObjectDriver(MovableObject* movable, World& world) :
+    m_movable(movable),
     m_world(&world)
 {
     m_index = m_world->add(movable);
 }
 
-MovableObject ObjectDriver::getObject()
+MovableObject* ObjectDriver::getObject()
 {
-    return m_world->getMovable(m_index);
+    return m_movable;
 }
 
 void ObjectDriver::accelerate(Vector a)
--- src/world.hpp
+++ src/world.hpp
@@ -35,6 +35,7 @@ namespace Flyy
     {
         VectorUnit m_x;
         VectorUnit m_y;
+
     public:
         Vector();
         Vector(VectorUnit magnitude, float angle);
@@ -76,6 +77,7 @@ namespace Flyy
     protected:
         PhysicalObject m_rect;
         float m_coefficientOfElasticity;
+
     public:
         PhysicalObject getRect() const;
         PositionUnit left() const;
@@ -83,8 +85,6 @@ namespace Flyy
         PositionUnit top() const;
         PositionUnit bottom() const;
         float getCoefficientOfElasticity() const;
-    protected:
-
     };
 
     class Wall :
@@ -101,6 +101,7 @@ namespace Flyy
         unsigned m_mass;
         Vector m_velocity;
         float m_coefficientOfResistance;
+
     public:
         MovableObject(){};
         MovableObject(unsigned mass, float cOfResistance,
@@ -119,49 +120,53 @@ namespace Flyy
 
     class World
     {
-        std::vector<Wall> m_walls;
-        std::vector<MovableObject> m_movables;
+        std::vector<Wall*> m_walls;
+        std::vector<MovableObject*> m_movables;
 
         float m_coefficientOfSlowdown;
 
         Vector m_gravityAcceleration;
         float m_coefficientOfResistanceOfEnvironment;
         bool m_hasBeenChanged;
+
     public:
         World(float cOfEnvResistance = 1,
               float gi = 50);
         void update();
-        void setWalls(std::vector<Wall> walls);
-        void setMovables(std::vector<MovableObject> movables);
-        int add(Wall wall);
-        int add(MovableObject movable);
-        const std::vector<Wall>& getWalls() const;
-        const std::vector<MovableObject>& getMovables() const;
-        Wall getWall(int index) const;
-        MovableObject getMovable(int index) const;
+        void setWalls(std::vector<Wall*> walls);
+        void setMovables(std::vector<MovableObject*> movables);
+        int add(Wall* wall);
+        int add(MovableObject* movable);
+        const std::vector<Wall*>& getWalls() const;
+        const std::vector<MovableObject*>& getMovables() const;
+        Wall* getWall(int index) const;
+        MovableObject* getMovable(int index) const;
         void accelerate(int index, Vector a);
         bool hasBeenChanged();
         void changesHasBeenSeen();
         void setCoefficientOfSlowdown(short cOfSlowdown);
         short getCoefficientOfSlowdown();
+
     private:
-        void updatePosition(MovableObject& object);
-        Vector getEnvResistanceEffects(const MovableObject& object);
-        void objectCollisionDetection(MovableObject& object);
-        std::vector<MovableObject> getNearbyObjects(const MovableObject& object);
-        std::vector<Wall> getNearbyWalls(const MovableObject& object);
-        void calculateCollisionEffects(MovableObject& o1,
-                                       MovableObject& o2);
-        void calculateCollisionEffects(MovableObject& o, Wall& w);
+        void updatePosition(MovableObject* object);
+        Vector getEnvResistanceEffects(const MovableObject* object);
+        void objectCollisionDetection(MovableObject* object);
+        std::vector<MovableObject*> getNearbyObjects(const MovableObject* object);
+        std::vector<Wall*> getNearbyWalls(const MovableObject* object);
+        void calculateCollisionEffects(MovableObject* o1,
+                                       MovableObject* o2);
+        void calculateCollisionEffects(MovableObject* o, Wall* w);
     };
 
     class ObjectDriver
     {
         int m_index;
+        MovableObject* m_movable;
         World* m_world;
+
     public:
-        ObjectDriver(MovableObject movable, World& world);
-        MovableObject getObject();
+        ObjectDriver(MovableObject* movable, World& world);
+        MovableObject* getObject();
         void accelerate(Vector a);
     };
 }
